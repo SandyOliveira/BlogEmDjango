3bbe8af854aafabeb1266fc9b8399d1991ce4f57
SandyOliveira <sandyoliveira828@gmail.com>
2020-05-26 02:10:13 -0300

    criação das apps & manager & querysets

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
 
 from .models import Post
-admin.site.register(Post)
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'autor', 'publicado','status')
+    list_filter = ('status', 'criado', 'publicado', 'autor')
+    raw_id_fields = ('autor',)
+    date_hierarchy = ('publicado')
+    search_fields = ('title', 'conteudo')
+    prepopulated_field = {'slug': ('title',)}
--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+#class PublishedManager(models.Manager):
+ #   def get_queryset(self):
+
 
 class Post (models.Model):
     STATUS =(
